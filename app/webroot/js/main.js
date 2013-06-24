bbd0705c7d632de228153c81fea392e4c6a20c3c
Thomas Degry <degry.thomas@gmail.com>
2013-06-24 17:40:57 +0200

    Added ratings aerage calculation

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -684,9 +684,7 @@ var Settings =(function () {
 
     var Settings = function () {
 
-        // this.URI = 'http://localhost/Devine/_MAMP_JAAR2/_SEM2/MAIV/mrburger/maiv-site';
-        this.URI = 'http://localhost/mrburger-php';
-        this.API = 'http://ksjkuurne.be/FOOD/api';
+        this.URI = 'http://localhost/Devine/_MAMP_JAAR2/_SEM2/MAIV/mrburger/maiv-site';
         //this.api = 'http://192.168.2.8/maiv_oostende/api/';
         //this.api = 'http://192.168.2.4/rolstende/api/';
     };
